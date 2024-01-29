4917af9dffe5aae51a4ae2745810756cdd2f8179
rlszabo966 <rlszabo966@gmail.com>
2024-01-29 01:33:59 +0100

    adding carousel basic blocks

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,11 @@
+import Carousel from "@/components/Carousel/Carousel";
 import { memo } from "react";
 
 function Page() {
     return (
-        <div className={`grid grid-rows-auto md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 sm:grid-cols-1 xs:grid-cols-1 min-h-[200px] h-[100vh] w-[100%] overflow-y-scroll`}>
-            <div className="h-[100vh] w-full bg-rgba self-center justify-self-center flex flex-col items-center justify-center min-h-[600px]">
-                Reverse Engineered Website: Nuphy Keyboards (2023)
-                <div>image</div>
-                <div>tech</div>
-                <div>I've reverse engineered the NuPhy Keyboards brand website, up until the product pages/checkout, etc. as an exercize to get more comfortable with React.</div>
-                <div>link to live</div>
-                <div>link to code</div>
-            </div>
-            <div className="h-[100vh] w-full bg-rgba self-center justify-self-center flex flex-col items-center justify-center min-h-[600px]">
-                Mamontov (MVX) Productions
-            </div>
-        </div>
+        <>
+            <Carousel />
+        </>
     );
 }
 
