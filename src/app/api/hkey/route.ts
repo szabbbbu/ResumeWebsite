2b33ab417c8dc7e0c795236818d2fb8c1b48929a
rlszabo966 <rlszabo966@gmail.com>
2024-02-13 14:24:44 +0100

    add api route for hcaptcha

---
+++ src/app/api/hkey/route.ts
@@ -0,0 +1,9 @@
+
+import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
+export async function GET() {
+    console.log("getting key: ", process.env.HCAPTCHA_SITE_KEY)
+    return NextResponse.json({hkey: process.env.HCAPTCHA_SITE_KEY})
+
+}
+
--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,22 +1,37 @@
 "use client"
+import dynamic from "next/dynamic";
 import { useState, useRef, useEffect } from "react";
-import HCaptcha from "@hcaptcha/react-hcaptcha";
+const HCaptcha = dynamic(() => import('@hcaptcha/react-hcaptcha'), { ssr: false });
 import GithubIcon from "./Icons/GithubIcon";
-import { useAppContext } from "@/contexts/useAppContext";
 import { showContentIfMobileMenuHidden } from "./util/HideIfMobile";
 
 export default function Contact () {
     const [emailCopied, setEmailCopied] = useState<boolean>(false);
     const [captchaSolved, setCaptchaSolved] = useState<boolean>(false);
     const [activateCaptcha, setActivateCaptcha] = useState<boolean>(false);
-    const captchaRef = useRef(null);
+    const [hCaptchaKey, setHCaptchaKey] = useState<string | null>(null)
+
+    // const captchaRef = useRef(null);
     const showContent = showContentIfMobileMenuHidden();
+
+    async function getHCaptchaKey() {
+        const resp = await fetch("/api/hkey");
+        const j = await resp.json();
+        return j.hkey;
+    }
+
+    useEffect(() => {
+        getHCaptchaKey().then(k => setHCaptchaKey(k));
+    }, []);
+
     if (showContent)
         return (
                 <div className="w-full h-[100vh] flex flex-col items-center justify-center">
                     <h1
                     className="p-4 rounded add-blur border text-xl text-center w-[90%]"
-                    >Want to commission me for your next project?</h1>
+                    >
+                        Want to hire me for your next project?
+                    </h1>
                     <div  className=" w-full h-[40%] grid grid-cols-1 grid-rows-[1fr,2fr,2fr] justify-center items-center">
                         <div className={`xs:w-[90%] md:w-[80%] h-fit p-4 mt-3 justify-self-center flex flex-wrap items-center justify-evenly add-blur border rounded ${captchaSolved ? "fade-in" : "start-state"}`}>
                         
@@ -50,33 +65,33 @@ export default function Contact () {
                     <div className={`h-fit w-full grid grid-cols-1 grid-rows-[1fr,2fr] rounded p-4`}>
                         <button
                         onClick={() => {
-                            console.log("CLICKED!")
+                            // console.log("CLICKED!")
                             setActivateCaptcha(true)
                         }}
                         className=" w-[60%] justify-self-center rounded border add-blur uppercase active:bg-[rgba(50,50,50,0.5)] hover:bg-[rgba(50,50,50,0.3)]"
                             >
                                 get in touch
                         </button>
-                        <div className={`w-full h-[200px] flex flex-col items-center ${activateCaptcha? "fade-in z-0" : "start-state -z-10"} `}>
-                        <p className="text-lg my-2 add-blur">But Wait! Are you Human???</p>
-                        <HCaptcha
-                            ref={captchaRef}
-                            
-                            // TODO: use getServerSideProps to get this env variable
-                            // sitekey={}
-                            sitekey="63cec785-a99d-4a00-b81f-207caccae4b3"
-                            theme="dark"
-                            onLoad={() => console.log("loaded")}
-                            onVerify={(token, ekey) => {
-                                setCaptchaSolved(true);
-                            }}
-                        />
-
+                        <div className={`w-full h-[200px] flex flex-col items-center ${activateCaptcha && !captchaSolved? "fade-in z-0" : "start-state -z-10"} `}>
+                        <p className="text-lg my-2 add-blur">
+                            But Wait! Are you Human???
+                        </p>
+                        {hCaptchaKey && 
+                            (
+                                <HCaptcha
+                                    sitekey={hCaptchaKey}
+                                    theme="dark"
+                                    onLoad={() => console.log("loaded")}
+                                    onVerify={(token, ekey) => {
+                                        setCaptchaSolved(true);
+                                    }}
+                                />
+                            )
+                        }
                         </div>
                     </div>
                     </div>    
                 </div> 
         );
-
     return null;
 }
