261ba43784e79c4a0475cc308c8ab3bc3515975f
rlszabo966 <rlszabo966@gmail.com>
2024-02-15 14:18:09 +0100

    test fix 3

--- src/app/api/hkey/route.ts
+++ src/app/api/hkey/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 export const dynamic = "force-dynamic";
 export async function GET() {
     // console.log("getting key: ", process.env.HCAPTCHA_SITE_KEY)
-    return NextResponse.json({hkey: process.env.HCAPTCHA_SITE_KEY as string});
+    return NextResponse.json({hkey: });
 }
 
